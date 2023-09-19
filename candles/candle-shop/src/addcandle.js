2608d9f07ef345dbab07269f24b03762928ca585
Asalagi <mypaintedfrog@yahoo.com>
2023-09-19 16:30:34 -0500

    lets try this

--- candles/candle-shop/src/addcandle.js
+++ candles/candle-shop/src/addcandle.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function AddCandle() {
     const [candle, setCandle] = useState({
-            canlde_name: '',
+            candle_name: '',
             candle_scent: '',
             candle_size: '',
             candle_price: '',
@@ -11,30 +11,35 @@ function AddCandle() {
         });
 
     const handleChange = (e) => {
-        setCandle(e.target.value);
+        setCandle({...candle, [e.target.name]: e.target.value});
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    }
-
-    axios.post('localhost:3001/candles', { candle })
-        .then(res => {
-            console.log(res.data)
+        axios.post("http://localhost:3001/candles", {
+            candle_name: candle.candle_name,
+            candle_scent: candle.candle_scent,
+            candle_size: candle.candle_size,
+            candle_price: candle.candle_price,
+            candle_summary: candle.candle_summary,
+        })
+        .then(response => {
+            console.log('a candle was added', response);
         })
         .catch(error => {
-            console.error(error);
+            console.log("there was an error", error);
         });
+    };
 
     return (
         <div>
             <h1>Add A Candle</h1>
             <form onSubmit={handleSubmit}>
-                <label>Candle Name </label><input type="text" name="candle name" onChange={handleChange} /><br/>
-                <label>Candle Scent </label><input type="text" name="candle scent" onChange={handleChange} /><br/>
-                <label>Candle Size </label><input type="text" name="candle size" onChange={handleChange} /><br/>
-                <label>Candle Price </label><input type="text" name="candle price" onChange={handleChange} /><br/>
-                <label>Candle Summary </label><textarea name="candle summary" onChange={handleChange} /><br/>
+                <label>Candle Name </label><input type="text" name="candle name" onChange={handleChange}/><br/>
+                <label>Candle Scent </label><input type="text" name="candle scent" onChange={handleChange}/><br/>
+                <label>Candle Size </label><input type="text" name="candle size" onChange={handleChange}/><br/>
+                <label>Candle Price </label><input type="text" name="candle price" onChange={handleChange}/><br/>
+                <label>Candle Summary </label><textarea name="candle summary" onChange={handleChange}/><br/>
                 <button type="submit">Add Candle</button>
             </form>
             
